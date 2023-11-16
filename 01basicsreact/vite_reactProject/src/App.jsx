874af8126b863a5dbab542d1f738fa3ea388b208
Aaditya Deshmukh <iamaaditya0109@gmail.com>
2023-11-16 15:23:51 +0530

    react_1 project

---
+++ 01basicsreact/vite_reactProject/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Wish from './helloworld'
+
+function App() {
+  const [count, setCount] = useState(0)
+return (
+  <Wish/>
+)
+
+}
+
+export default App
---
+++ 01basicsreact/vite_reactProject/src/helloworld.jsx
@@ -0,0 +1,5 @@
+function Wish()
+{
+    return (<h1>hello react </h1>)
+}
+export default Wish;
